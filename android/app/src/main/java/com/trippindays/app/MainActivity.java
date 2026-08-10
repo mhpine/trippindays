0368c8bf4deb7b95966bc4ec073f2e0306ddd547
Mitchekk Pine <cpicklescomedy@gmail.com>
2026-08-10 12:55:32 -0700

    Add smart app download and mobile navigation

--- android/app/src/main/java/com/trippindays/app/MainActivity.java
+++ android/app/src/main/java/com/trippindays/app/MainActivity.java
@@ -1,5 +1,32 @@
 package com.trippindays.app;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private static final int MICROPHONE_PERMISSION_CODE = 1001;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+        ) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    MICROPHONE_PERMISSION_CODE
+            );
+        }
+    }
+}
